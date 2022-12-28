c041c0bebc7247e724f2e7e954a48cf69824e270
Luka Kalinovcic <kalinovcic@gmail.com>
2022-12-28 20:40:06 +0100

    Advent of Code 2022: Day 16 redo without the hack

--- solutions/src/day16.rs
+++ solutions/src/day16.rs
@@ -1,8 +1,8 @@
-type LineData = (String, i32, Vec<String>);
+type LineData = (String, i16, Vec<String>);
 type Ids = std::collections::HashMap<String, usize>;
 
 struct Graph {
-  data: Vec<(i32, Vec<usize>)>,
+  data: Vec<(i16, Vec<usize>)>,
 }
 
 impl Graph {
@@ -11,7 +11,6 @@ impl Graph {
     for (name, flow, adj) in lines {
       let id = *ids.get(name).unwrap();
       data[id].0 = *flow;
-      data[id].1.push(id);
       for name in adj {
         data[id].1.push(*ids.get(name).unwrap());
       }
@@ -20,63 +19,42 @@ impl Graph {
   }
 }
 
-type Memo = std::collections::HashMap<(u8, u8, u8, u64), i32>;
+type Memo = multiarray::Array3D<i16>;
 
-fn solve(
-  graph: &Graph,
-  t: usize,
-  p1: usize,
-  p2: usize,
-  mask: usize,
-  add: i32,
-  use_pruning: bool,
-  memo: &mut Memo,
-) -> i32 {
+fn solve(graph: &Graph, t: usize, p: usize, mask: usize, memo: &mut Memo) -> i16 {
   if t == 0 {
     return 0;
   }
-  // Dirty hack!
-  if use_pruning && t >= 10 && t <= 20 && add < (200 - t as i32 * 8) {
-    return 0;
-  }
-  let memo_key = (t as u8, p1 as u8, p2 as u8, mask as u64);
-  if let Some(result) = memo.get(&memo_key) {
-    return *result;
+  {
+    let cached = memo[[t, p, mask]];
+    if cached >= 0 {
+      return cached;
+    }
   }
 
   let mut result = 0;
-  for np1 in &graph.data[p1].1 {
-    if *np1 == p1 && (graph.data[p1].0 == 0 || (mask >> p1) & 1 > 0) {
-      continue;
-    }
-    let nm = if *np1 == p1 { mask | (1 << p1) } else { mask };
-    let na = add + if *np1 == p1 { graph.data[p1].0 } else { 0 };
-    for np2 in &graph.data[p2].1 {
-      if *np2 == p2 && (graph.data[p2].0 == 0 || (nm >> p2) & 1 > 0) {
-        continue;
-      }
-      let nm = if *np2 == p2 { nm | (1 << p2) } else { nm };
-      let na = na + if *np2 == p2 { graph.data[p2].0 } else { 0 };
-      result = std::cmp::max(
-        result,
-        add + solve(graph, t - 1, *np1, *np2, nm, na, use_pruning, memo),
-      );
-    }
+  if (mask >> p) & 1 > 0 {
+    let add = (t - 1) as i16 * graph.data[p].0;
+    result = add + solve(graph, t - 1, p, mask ^ (1 << p), memo);
   }
-  memo.insert(memo_key, result);
+  for np in &graph.data[p].1 {
+    result = std::cmp::max(result, solve(graph, t - 1, *np, mask, memo));
+  }
+
+  memo[[t, p, mask]] = result;
   result
 }
 
 pub fn doit(lines: &Vec<String>) -> (String, String) {
   let re = regex::Regex::new(r"^Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)$")
     .unwrap();
-  let mut lines: Vec<LineData> = lines
+  let lines: Vec<LineData> = lines
     .iter()
     .map(|line| {
       let m = re.captures(line).unwrap();
       (
         m.get(1).unwrap().as_str().to_string(),
-        m.get(2).unwrap().as_str().parse::<i32>().unwrap(),
+        m.get(2).unwrap().as_str().parse::<i16>().unwrap(),
         m.get(3)
           .unwrap()
           .as_str()
@@ -86,23 +64,33 @@ pub fn doit(lines: &Vec<String>) -> (String, String) {
       )
     })
     .collect();
-  lines.push(("NOOP1".to_string(), 0, vec!["NOOP2".to_string()]));
-  lines.push(("NOOP2".to_string(), 0, vec!["NOOP1".to_string()]));
 
   let mut ids = Ids::new();
-  for (name, _, _) in lines.iter() {
-    let id = ids.len();
-    ids.insert(name.clone(), id);
+  for pass in 0..2 {
+    for i in 0..lines.len() {
+      if pass == 0 && lines[i].1 > 0 || pass == 1 && lines[i].1 == 0 {
+        let id = ids.len();
+        ids.insert(lines[i].0.clone(), id);
+      }
+    }
   }
+  let non_zero = lines.iter().filter(|d| d.1 > 0).count();
+  let full_mask = (1 << non_zero) - 1;
+
   let start = *ids.get("AA").unwrap();
-  let noop = *ids.get("NOOP1").unwrap();
-
   let graph = Graph::new(&lines, &ids);
-  let mut memo = Memo::new();
-  let part1 = solve(&graph, 30, start, noop, 0, 0, false, &mut memo);
 
-  let use_pruning = lines.len() > 30;
-  let part2 = solve(&graph, 26, start, start, 0, 0, use_pruning, &mut memo);
+  let mut memo = Memo::new([31, ids.len(), (1 << non_zero)], -1);
+  let part1 = solve(&graph, 30, start, full_mask, &mut memo);
+
+  let mut part2 = 0;
+  for mask1 in 0..(1 << (non_zero - 1)) {
+    let mask2 = full_mask ^ mask1;
+    part2 = std::cmp::max(
+      part2,
+      solve(&graph, 26, start, mask1, &mut memo) + solve(&graph, 26, start, mask2, &mut memo),
+    )
+  }
 
   (part1.to_string(), part2.to_string())
 }
